d443de7d80ac94f8abe6dd24963cfb709d75d3a0
soeunSim <629943@naver.com>
2025-02-14 19:22:42 +0900

    chore: prettier 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 function App() {
-
   return (
     <>
       <h1 className="text-3xl font-bold underline text-mainBlack">
         Hello world!
       </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
