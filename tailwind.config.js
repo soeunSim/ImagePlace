0bedfc51abbab4b41a4fc5379577722d60241730
soeunSim <629943@naver.com>
2025-02-14 19:16:34 +0900

    chore: tailwindcss 설정

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "mainBlack": "#000000",
+        "subGray": "#333333",
+      }      
+    },
+  },
+  plugins: [],
+}
+
