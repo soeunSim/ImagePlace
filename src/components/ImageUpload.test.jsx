0f01d5edacb129c79005936ce257342be03eee9c
soeunSim <629943@naver.com>
2025-06-02 12:11:17 +0900

    refactor: ImageUpload 테스트를 ImageUpload.test.jsx로 리네임 및 위치 변경, import 경로 수정

--- src/components/ImageUpload.test.jsx
+++ src/components/ImageUpload.test.jsx
@@ -1,6 +1,6 @@
 import { describe, expect, render, test } from "vitest";
 
-import ImageUpload from "../component/ImageUpload";
+import ImageUpload from "./ImageUpload";
 
 describe("ImageUpload 컴포넌트 렌더링", () => {
   test("드래그앤드롭 input과 파일 업로드 input이 모두 존재하는가", () => {
